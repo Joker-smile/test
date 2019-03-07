f9617543d4e41998332777a6449b86b412edabe1
joker <zai1020733278@gmail.com>
2019-03-07 14:52:55 +0800

    帮助法

---
+++ bootstrap/helpers.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!function_exists('current_admin')) {
+
+    function current_admin()
+    {
+        if (Auth::guard('web')->check()) {
+            return Auth::guard('web')->user();
+        }
+
+        return false;
+    }
+}
+
+if (!function_exists('current_user')) {
+
+    function current_user()
+    {
+        if (Auth::guard('api')->check()) {
+            return Auth::guard('api')->user();
+        }
+
+        return false;
+    }
+}
+
+function format_money($amount)
+{
+    return number_format($amount/100 ?? 0, 2, '.', '');
+}
+
+
