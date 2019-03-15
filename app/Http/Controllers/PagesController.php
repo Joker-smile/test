a130dfe2419f8021bc234ef80e27f56bff766fbe
joker <zai1020733278@gmail.com>
2019-03-15 14:32:59 +0800

    fix

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,7 +13,7 @@ class PagesController extends Controller
         $data['backUrl'] = 'https://loveinyeu.cn/payment/back/';
         $data['noticeUrl'] = 'https://loveinyeu.cn/payment/notify/';
         $data['methods'] = 'Credit Card';
-        $data['order_number'] = '20190115081630370274';
+        $data['order_number'] = '20190115081630370275';
         $data['order_currency'] = 'USD';
         $data['order_amount'] = 100;
         $data['billing_firstName'] = 'Frances L';
