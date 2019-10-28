c5b671a15b38ac2b57fbdec8fff2ccce5d1a6529
joker <zai1020733278@gmail.com>
2019-10-28 17:29:31 +0800

    fix

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,16 +8,6 @@ class PagesController extends Controller
 {
     public function index()
     {
-
-        $client = new Client();
-        $r      = $client->request('POST', 'http://admin.agent.com/api/v1/business/mabangs/sync/order', [
-            'form_params' => [
-                'platform_order_id' => '59366381247265001350730',
-                'company_id'        => '100003'
-            ]
-        ]);
-        dd($r->getBody()->getContents());
-
-        return view('pages.home')->with(compact('data'));
+        return view('pages.home');
     }
 }
