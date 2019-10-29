888cae95af3008fded5b952fc495d06297367ed8
joker <zai1020733278@gmail.com>
2019-10-29 11:17:32 +0800

    fix

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PagesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        Log::info($request->getContent());
+        Log::info($request);
         return view('pages.home');
     }
 }
