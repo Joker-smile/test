c91a28de0332ecedd02c25a81e0d3c206b305588
joker <zai1020733278@gmail.com>
2019-11-05 14:36:59 +0800

    fix

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -17,13 +17,12 @@ class WeChatController extends Controller
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $app      = app('wechat.official_account');
-        $response = $app->server->serve();
+        $app = app('wechat.official_account');
+        $accessToken = $app->access_token;
+        $token = $accessToken->getToken(); // token 数组  token['access_token'] 字符串
+        $token = $accessToken->getToken(true);
 
-// 将响应输出
-        $response->send();
-
-        return $response;
+        return $token;
     }
 
     public function profile()
@@ -44,8 +43,6 @@ class WeChatController extends Controller
 
 // 已经登录过
         $user = $_SESSION['wechat_user'];
-
-        dd($user);
 
     }
 
