90f6640f2f8531f5dbf5c34ff9d5e30dadb7479e
joker <zai1020733278@gmail.com>
2019-11-06 14:40:42 +0800

    fix

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
 class PagesController extends Controller
 {
     public function index()
     {
         return view('index');
+    }
+
+    public function getRequest(Request $request)
+    {
+        Log::info($request->getContent());
+        Log::info($request->get('store'));
     }
 }
---
+++ database/migrations/2019_07_08_091957_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateUsersTable.
+ */
+class CreateUsersTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('openid');
+            $table->string('nickname');
+            $table->string('sex');
+            $table->string('city');
+            $table->string('province');
+            $table->string('country');
+            $table->string('avatar');
+            $table->string('access_token')->default('');
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+}
