0c92b48da223fb0ae4f95eb61b7c44a26bea77e8
joker <zai1020733278@gmail.com>
2019-11-08 11:54:21 +0800

    fix

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,7 +9,7 @@ class PagesController extends Controller
 {
     public function index()
     {
-        return view('index');
+        return 'Hello world!';
     }
 
     public function getRequest(Request $request)
